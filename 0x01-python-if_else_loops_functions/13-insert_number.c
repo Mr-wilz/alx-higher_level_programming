acac8bd1340ca1e541cbe2a398e0938e987f7aff
Mr-wilz <juiciwhilf@gmail.com>
2023-11-28 21:18:49 -0800

    13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -14,24 +14,19 @@ listint_t *insert_node(listint_t **head, int number)
 {
 	listint_t *new_node, *traverse;
 	unsigned int idx = 0, i = 0;
-	/* if there is no list return null */
+       
 	if (head == NULL)
 		return (NULL);
-	/* create the new node */
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	/* access the n field of the new_node and initialize it as n */
 	new_node->n = number;
-	/* check if idx = 0 */
 	if (idx == 0)
 	{
-		/* access the next field of new_node and assign it as first node */
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	/* make traverse be the value at head */
 	traverse = *head;
 	while (i != idx - 1 && traverse != NULL)
 	{
