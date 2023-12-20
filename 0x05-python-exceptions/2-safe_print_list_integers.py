d32a3d74eff55d5ae1c6e8c78c471840c02c6852
Mr-wilz <juiciwhilf@gmail.com>
2023-12-19 18:22:23 -0800

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
+
 def safe_print_list_integers(my_list=[], x=0):
+    """
+    Prints the first x elements of a list and only integers.
+    my_list can contain any type (integer, string, etc.)
+    You have to use try: / except:
+    You have to use "{:d}".format() to print as integer.
+    You are not allowed to import any module.
+    All integers have to be printed on the same line followed by a new line,
+    - other type of value in the list must be skipped (in silence).
+    You are not allowed to use len().
+
+        Args:
+            x: Number of elements to access in my_list.
+
+        Returns: The real number of integers printed.
+    """
     count = 0
-"""
-prints the first x elements of a list and only integers
-
-Args:
-    x: number of elements to access
-    Return: the real number of integers printed
-"""
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end='')
-        except TypeError:
-            pass
-        except ValueError:
-            pass
+    for item in range(x):
+        if isinstance(my_list[item], int):
+            try:
+                print(my_list[item], end='')
+            except IndexError:
+                break
         else:
-            count += 1
-
+            continue
+        count += 1
     print()
-    return (count)
+    return count
