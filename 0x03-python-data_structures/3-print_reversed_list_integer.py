ecfebdaad85c2713a8b8a1e0591601f3098fa349
Mr-wilz <juiciwhilf@gmail.com>
2023-12-30 19:19:47 -0800

    3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+    """function that prints a list in reversed order
+    one ineger per line
+    """
+
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{}".format(my_list[i]))
