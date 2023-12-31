a7af523f3d4a24cb05854a708e627aa15070db7c
Mr-wilz <juiciwhilf@gmail.com>
2023-12-31 01:19:03 -0800

    9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    """Function that finds the biggest integer in a list.
-    - my_list: input list of integers
-    - Return: max integer or none if list is empty
+    """function that returns biggest integer in list
+
+    If the list is empty, return None
     """
 
-    if len(my_list) == 0:
-        return None
-    if len(my_list) > 0:
-        max_int = my_list[0]
-        for num in  my_list:
-            if num > max_int:
-                max_int = num
-        return max_int
+    list_length = len(my_list)
+
+    if list_length == 0:
+        return (None)
+
+    my_list.sort()
+
+    return my_list[-1]
