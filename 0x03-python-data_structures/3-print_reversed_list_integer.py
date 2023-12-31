194cb520b1bf0d30ef5437ebc17685762968d09a
Mr-wilz <juiciwhilf@gmail.com>
2023-12-30 19:24:43 -0800

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,13 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 def print_reversed_list_integer(my_list=[]):
-    """function that prints a list in reversed order
-    one ineger per line
+    """function that prints list in reverse
+
+    One integer per line
+    You are not allowed to import any module
+    You can assume that the list only contains integers
+    You are not allowed to cast integers into strings
+    You have to use str.format() to print integers
     """
 
     if my_list:
