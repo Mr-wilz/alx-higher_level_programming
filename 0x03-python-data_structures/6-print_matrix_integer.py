5d4e37fe4deda1fe004dfd27853d0dac68145fc9
Mr-wilz <juiciwhilf@gmail.com>
2023-12-30 22:03:21 -0800

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    """function that prints a matrix of integers
+    -matrix (list of list): input matrix
+    """
+
+    for row in matrix;
+        for elements in  row:
+            print(element, end=" ")
+        print()
