78376b39cf9b0c4b67ef0554d3e882d4c9213d21
Mr-wilz <juiciwhilf@gmail.com>
2023-12-30 20:48:48 -0800

    5-no_c.py

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    """
+    function that removes all c characters from a string    
+    """
+    new_string = ""
+    for char in my_string:
+        if char.lower() != 'c':
+            new_string += char
+    return new_string
