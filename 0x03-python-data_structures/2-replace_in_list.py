b13b524ba610851f5ce5342f55f4f7bf101af87e
Mr-wilz <juiciwhilf@gmail.com>
2023-12-30 18:55:11 -0800

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,15 +5,10 @@ def replace_in_list(my_list, idx, element):
        if the idx is out of range return original list without modification
     """
 
-
     if idx < 0:
         return (my_list)
-
     length = len(my_list)
-
     if idx > length - 1:
         return (my_list)
-
     my_list[idx] = element
-
     return (my_list)
