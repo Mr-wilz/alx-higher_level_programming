6ac4a3b6a1e8c969a6b5f9f0b2e20c5bc28466cf
Mr-wilz <juiciwhilf@gmail.com>
2023-12-31 00:54:06 -0800

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,8 +1,18 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        return (0, None)
-    if sentence:
-        length = len(sentence)
-        first_char = sentence[0]
-        return (length, first_char)
+    """function that returns tuple with the length of a str and its first,
+    character
+
+    If the sentence is empty, the first character should be equal to None
+    You are not allowed to import any module
+    """
+
+    sentence_length = len(sentence)
+
+    if sentence_length == 0:
+        new_tuple = (0, None)
+    else:
+        new_tuple = (sentence_length, sentence[0])
+
+    return new_tuple
