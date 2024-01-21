1c8f6c067f93b965cb0fd901f3fcccc0ba68c0d6
Mr-wilz <juiciwhilf@gmail.com>
2024-01-21 10:08:26 -0800

    6-print_sorted_dictionary.py

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,9 +4,6 @@ def print_sorted_dictionary(a_dictionary):
     """Prints a dictionary by ordered keys.
     You can assume that all keys are strings.
     Keys are sorted by alphabetic order.
-    Only sort keys of the first level (don’t sort keys of a dictionary inside the main dictionary).
-    Dictionary values can have any type.
-    You are not allowed to import any module.
     """
     sorted_keys = sorted(a_dictionary.keys())
 
