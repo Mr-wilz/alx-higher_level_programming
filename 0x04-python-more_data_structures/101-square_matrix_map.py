8a35e974353fceafbca7748e060635c5bc45875b
Mr-wilz <juiciwhilf@gmail.com>
2024-01-26 09:25:45 -0800

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 
 def square_matrix_map(matrix=[]):
-    """
-    computes the square of all integers in amtrix using map
-
-    args:
-    matrix  is a 2 dimensional array
-
-    Returns: a new matrix same size as matrix
-    each value should be the square of the value of the input
-    """
     if matrix is not None:
         return list(map(lambda x: list(map(lambda y: y**2, x)), matrix))
