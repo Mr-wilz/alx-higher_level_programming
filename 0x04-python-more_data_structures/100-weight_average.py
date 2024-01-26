dcfe837d483bde9e0d112401f51f1104528e50c6
Mr-wilz <juiciwhilf@gmail.com>
2024-01-25 19:45:45 -0800

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+ef weight_average(my_list=[]):
+    """
+    Returns the weighted average of all integers tuple (<score>, <weight>).
+
+    Args:
+        my_list: A list of tuples, each containing a score and its corresponding weight.
+
+    Returns:
+        The weighted average as a float. Returns 0 if the list is empty.
+    """
+    if not my_list:
+        return 0
+
+    total_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    if total_weight == 0:
+        return 0
+
+    return total_score / total_weight
