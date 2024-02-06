fdd00e722c432437140decdaca02dc4b4911619f
Mr-wilz <juiciwhilf@gmail.com>
2024-02-05 16:27:24 -0800

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Rectangle:
+    """
+    A class representing a rectangle.
+
+    This class is currently empty and can be used as a template for defining
+    rectangles with specific attributes and methods.
+
+    Attributes:
+    - No attributes defined yet.
+
+    Methods:
+    - No methods defined yet.
+    """
+    pass
+
