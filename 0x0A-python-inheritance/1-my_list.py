69723992cd7e409e8c6705cd63f6a6d733f221f0
Mr-wils <juiciwilf@gmail.com>
2024-03-07 19:29:38 -0800

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""a class Mylist that inherits from the built list"""
+
+class MyList(list):
+    """Class that inherits from the Base class list
+
+    Args:
+     list: list to be sorted
+     """
+
+    def print_sorted(self):
+        sorted_list = sorted(self)
+        print(sorted_list)
+
