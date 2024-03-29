3860e400deccd2ced64ae0f092d7e72e1780e5c1
Mr-wilz <juiciwhilf@gmail.com>
2024-03-28 21:53:50 -0700

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""takes a url, send a request and display response"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    body = response.text
+    if response.status_code >= 400:
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(body)"
