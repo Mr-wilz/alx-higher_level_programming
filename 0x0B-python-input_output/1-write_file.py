d5e680a43847d659a717f4f7a1c1637b845f9d5b
Wilz <124950007+Mr-wilz@users.noreply.github.com>
2024-04-04 15:14:22 +0100

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -16,4 +16,3 @@ def write_file(filename="", text=""):
     with open(filename, 'w', encoding="utf-8") as f:
         """This method returns the number of characters written to a file."""
         return f.write(text)
-
