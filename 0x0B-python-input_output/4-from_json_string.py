d724037f53e7aaf19d7d8bf7e7c1a160d1e5fb46
Mr-wilz <juiciwhilf@gmail.com>
2024-04-05 06:02:05 +0000

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""returns an obj rep by a a JSON string"""
+import json
+""" module containing the function to json atring"""
+
+def from_json_string(my_str):
+    """Returns the Python data structure represented by a JSON string.
+
+    Args:
+        my_str (str): JSON string.
+
+    Returns:
+        obj: Python data structure represented by the JSON string.
+    """
+    return json.loads(my_str)
