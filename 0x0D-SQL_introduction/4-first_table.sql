6da98714b4fcefebc86594ac5f361c146f3fac7d
Mr-wilz <juiciwhilf@gmail.com>
2024-09-01 21:10:42 -0700

    4-first_table.sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,2 @@
--- creates a table first_table in my DB
-CREATE TABLE IF NOT EXISTS 'first_table' (
-	'id' INT,
-	'name' VARCHAR(256)
-);
+-- creates a table called first_table in my current DB
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
